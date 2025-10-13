af3cb57193489b3d8f6affe0aadb15324134237d
Bibek Lama <devshering@gmail.com>
2025-10-13 16:10:57 +0200

    fix: updated navigations

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -1,4 +1,4 @@
-import { Building, FileText, Inbox, LayoutDashboard, Send, Users } from "lucide-react";
+import { Group, Inbox, LayoutDashboard, Send, ShoppingBag, Users } from "lucide-react";
 
 export const dashboardMenuItems = [
     {
@@ -9,36 +9,43 @@ export const dashboardMenuItems = [
     {
         title: "Invoices",
         icon: Inbox,
-        menus: [{
-            title: "Received Invoices",
-            icon: Inbox,
-            url: "/invoices?tab=received",
-        },
-        {
-            title: "Sent Invoices",
-            icon: Send,
-            url: "/invoices?tab=sent",
-        },
-        {
-            title: "All Invoices",
-            icon: FileText,
-            url: "/invoices?tab=all",
-        },
+        menus: [
+            {
+                title: "Received Invoices",
+                icon: Inbox,
+                url: "/invoices?tab=received",
+            },
+            {
+                title: "Sent Invoices",
+                icon: Send,
+                url: "/invoices?tab=sent",
+            },
+            {
+                title: "All Invoices",
+                icon: Group,
+                url: "/invoices?tab=all",
+            },
         ]
     },
     {
         title: "Contacts",
         icon: Inbox,
-        menus: [{
-            title: "Vendors",
-            icon: Building,
-            url: "/contacts?tab=vendors",
-        },
-        {
-            title: "Clients",
-            icon: Users,
-            url: "/contacts?tab=clients",
-        },
+        menus: [
+            {
+                title: "Vendors",
+                icon: ShoppingBag,
+                url: "/contacts?tab=vendors",
+            },
+            {
+                title: "Clients",
+                icon: Users,
+                url: "/contacts?tab=clients",
+            },
+            {
+                title: "All Contacts",
+                icon: Group,
+                url: "/contacts?tab=all",
+            },
         ]
     }
 ]
